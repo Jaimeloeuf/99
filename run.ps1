da7e796c8767151b7ddbd2f35655bbd0ba4f8074
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-04 00:33:21 +0800

    Updated path of the files used in "run" script     Updated after the source files are moved into the src/ directory.

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 # Powershell script to start all the services independantly
 
-start powershell {python public_service.py --port=6000}
-start powershell {python listing_service.py --port=6001}
-start powershell {python user_service.py --port=6002}
+start powershell {python ./src/public_service.py --port=6000}
+start powershell {python ./src/listing_service.py --port=6001}
+start powershell {python ./src/user_service.py --port=6002}
