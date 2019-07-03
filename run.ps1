c86ffe131f7c0b4cea35753415287edc2bc842a8
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-04 00:05:36 +0800

    Updated powershell script to include public_service

--- run.ps1
+++ run.ps1
@@ -1,4 +1,5 @@
 # Powershell script to start all the services independantly
 
-start powershell {python user_service.py --port=5000}
-start powershell {python listing_service.py --port=6000}
+start powershell {python public_service.py --port=6000}
+start powershell {python listing_service.py --port=6001}
+start powershell {python user_service.py --port=6002}
