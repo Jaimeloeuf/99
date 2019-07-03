a1213d5b56e250270d350bc1494f8d77f8c4bf34
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-03 11:40:18 +0800

    Updated and cleaned up scaffolded user_service: 1.  Updated the scaffolding code to finish implementing the routes for:     - Get All Users     - Create User
    2.  Removed all the unneccessary code for things like validation and more.

--- user_service.py
+++ user_service.py
@@ -23,9 +23,7 @@ class App(tornado.web.Application):
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS 'users' ("
             + "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-            + "user_id INTEGER NOT NULL,"
-            + "listing_type TEXT NOT NULL,"
-            + "price INTEGER NOT NULL,"
+            + "name TEXT NOT NULL,"
             + "created_at INTEGER NOT NULL,"
             + "updated_at INTEGER NOT NULL"
             + ");"
@@ -59,30 +57,15 @@ class UsersHandler(BaseHandler):
             self.write_json({"result": False, "errors": "invalid page_size"}, status_code=400)
             return
 
-        # Parsing user_id param
-        user_id = self.get_argument("user_id", None)
-        if user_id is not None:
-            try:
-                user_id = int(user_id)
-            except:
-                self.write_json({"result": False, "errors": "invalid user_id"}, status_code=400)
-                return
-
         # Building select statement
         select_stmt = "SELECT * FROM users"
-        # Adding user_id filter clause if param is specified
-        if user_id is not None:
-            select_stmt += " WHERE user_id=?"
         # Order by and pagination
         limit = page_size
         offset = (page_num - 1) * page_size
         select_stmt += " ORDER BY created_at DESC LIMIT ? OFFSET ?"
 
-        # Fetching users from db
-        if user_id is not None:
-            args = (user_id, limit, offset)
-        else:
-            args = (limit, offset)
+        # Create the SQL arguement tuple
+        args = (limit, offset)
         cursor = self.application.db.cursor()
         results = cursor.execute(select_stmt, args)
 
@@ -99,32 +82,21 @@ class UsersHandler(BaseHandler):
     @tornado.gen.coroutine
     def post(self):
         # Collecting required params
-        user_id = self.get_argument("user_id")
+        name = self.get_argument("name")
 
-        # @listing stuff
-        # listing_type = self.get_argument("listing_type")
-        # price = self.get_argument("price")
-
-        # Validating inputs
-        errors = []
-        user_id_val = self._validate_user_id(user_id, errors)
-        listing_type_val = self._validate_listing_type(listing_type, errors)
-        price_val = self._validate_price(price, errors)
+        # Create the microS timestamp
         time_now = int(time.time() * 1e6) # Converting current time to microseconds
-
-        # End if we have any validation errors
-        if len(errors) > 0:
-            self.write_json({"result": False, "errors": errors}, status_code=400)
-            return
 
         # Proceed to store the users in our db
         cursor = self.application.db.cursor()
+
         cursor.execute(
             "INSERT INTO 'users' "
-            + "('user_id', 'name', 'created_at', 'updated_at') "
-            + "VALUES (?, ?, ?, ?)",
-            (user_id_val, name, time_now, time_now)
+            + "('name', 'created_at', 'updated_at') "
+            + "VALUES (?, ?, ?)",
+            (name, time_now, time_now)
         )
+
         self.application.db.commit()
 
         # Error out if we fail to retrieve the newly created user
@@ -132,47 +104,15 @@ class UsersHandler(BaseHandler):
             self.write_json({"result": False, "errors": ["Error while adding listing to db"]}, status_code=500)
             return
 
-        listing = dict(
+        user = dict(
             id=cursor.lastrowid,
-            user_id=user_id_val,
-            listing_type=listing_type_val,
-            price=price_val,
+            name=name,
             created_at=time_now,
             updated_at=time_now
         )
 
-        self.write_json({"result": True, "listing": listing})
+        self.write_json({"result": True, "user": user})
 
-    def _validate_user_id(self, user_id, errors):
-        try:
-            user_id = int(user_id)
-            return user_id
-        except Exception as e:
-            logging.exception("Error while converting user_id to int: {}".format(user_id))
-            errors.append("invalid user_id")
-            return None
-
-    def _validate_listing_type(self, listing_type, errors):
-        if listing_type not in {"rent", "sale"}:
-            errors.append("invalid listing_type. Supported values: 'rent', 'sale'")
-            return None
-        else:
-            return listing_type
-
-    def _validate_price(self, price, errors):
-        # Convert string to int
-        try:
-            price = int(price)
-        except Exception as e:
-            logging.exception("Error while converting price to int: {}".format(price))
-            errors.append("invalid price. Must be an integer")
-            return None
-
-        if price < 1:
-            errors.append("price must be greater than 0")
-            return None
-        else:
-            return price
 
 # /users/ping
 class PingHandler(tornado.web.RequestHandler):
@@ -182,9 +122,9 @@ class PingHandler(tornado.web.RequestHandler):
 
 def make_app(options):
     return App([
-        (r"/users", UsersHandler),
-        # (r"/users/{id}", PingHandler),
         (r"/users/ping", PingHandler),
+        (r"/users", UsersHandler),
+        # (r"/users/{id}", PingHandler)
     ], debug=options.debug)
 
 if __name__ == "__main__":
